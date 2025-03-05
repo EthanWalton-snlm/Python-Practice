0eb3da8fae41966949276fb15bc1a9dee3fe905c
EthanWalton-snlm <ethan.walton@sanlam.co.za>
2025-03-05 16:12:39 +0200

    Day 11 Final

--- Day 10/assignment.py
+++ Day 10/assignment.py
@@ -2,3 +2,16 @@
 # Write a comparison in terms of scope on keywords
 # global vs nonlocal
 # with examples
+
+
+global color
+color = "blue"
+
+
+def sky_color():
+    color = "green"
+    global colour
+    return f"The sky is {color}"
+
+
+print(sky_color())
